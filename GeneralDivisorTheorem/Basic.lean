2d0bf2e2b0a57904e74ee16820a51b810d29829c
dan_hawkley <consensusmechanics@gmail.com>
2026-09-05 17:01:51 -0600

    Update copyright information in Basic.lean

--- GeneralDivisorTheorem/Basic.lean
+++ GeneralDivisorTheorem/Basic.lean
@@ -1,6 +1,7 @@
 /-
-Copyright (c) 2026 thinkthoughts. All rights reserved.
+Copyright (c) 2026 Dan Hawkley. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Dan Hawkley
 -/
 
 module
